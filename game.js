bf876ef56f1ef2ffc1af4a2237e8e7946de9ec01
Vitor Duarte Chaves <vitord.chaves@gmail.com>
2025-09-09 16:46:51 -0300

    Atualização inimigo e Projeteis

--- game.js
+++ game.js
@@ -20,9 +20,38 @@ const game = new Phaser.Game(config);
 
 let player;
 let cursors;
+let enemy;
+let fogo;
 
-function preload() {}
+function preload() {
+    this.load.image('player', 'personagem.png');
+    this.load.image('enemy', 'vilao.png');
+    this.load.image('fogo', 'fogo.png');
+}
 
-function create() {}
+function create() {
+    player = this.physics.add.sprite(400, 300, 'player');
+    player.setScale(0.2);
+    enemy = this.physics.add.sprite(200, 500, 'enemy');
+    enemy.setScale(0.2);
+    sonic_boom = this.physics.add.group({
+        defaultKey: "fogo",
+        maxSize: 5,
+        runChildUpdate: true,
+    })
+    cursors = this.input.keyboard.createCursorKeys();
+}
 
-function update() {}
+function update() {
+    if (cursors.left.isDown) {
+        player.x -= 3;
+    } else if (cursors.right.isDown) {
+        player.x += 3;
+    }
+
+    if(cursors.up.isDown){
+        player.y -= 3;
+    } else if (cursors.down.isDown) {
+        player.y += 3;
+    }
+}
